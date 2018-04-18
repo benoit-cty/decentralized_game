2854cd73418cc65b12fb3c332b9f5f347da56b68
Benoit <benoit.courty.ext@francetv.fr>
2018-04-18 10:10:28 +0200

    Adding front

---
+++ src/js/app.js
@@ -0,0 +1,76 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: function() {
+    // Load pets.
+    $.getJSON('../pets.json', function(data) {
+      var petsRow = $('#petsRow');
+      var petTemplate = $('#petTemplate');
+
+      for (i = 0; i < data.length; i ++) {
+        petTemplate.find('.panel-title').text(data[i].name);
+        petTemplate.find('img').attr('src', data[i].picture);
+        petTemplate.find('.pet-breed').text(data[i].breed);
+        petTemplate.find('.pet-age').text(data[i].age);
+        petTemplate.find('.pet-location').text(data[i].location);
+        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+
+        petsRow.append(petTemplate.html());
+      }
+    });
+
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    /*
+     * Replace me...
+     */
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('Adoption.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+      // Set the provider for our contract
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markAdopted();
+    });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  },
+
+  markAdopted: function(adopters, account) {
+    /*
+     * Replace me...
+     */
+  },
+
+  handleAdopt: function(event) {
+    event.preventDefault();
+
+    var petId = parseInt($(event.target).data('id'));
+
+    /*
+     * Replace me...
+     */
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
