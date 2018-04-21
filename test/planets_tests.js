8ddd9e354d1cc20d29ef1c4c8e094930e54b64ac
Benoit <benoit.courty.ext@francetv.fr>
2018-04-21 14:28:27 +0200

    test

--- test/planets_tests.js
+++ test/planets_tests.js
@@ -33,7 +33,7 @@ contract('ERC721Planet', function(accounts) {
       ).then(function(res) {
             return instance.ownerOf(1).then(function(owner){
             //console.log("owner=" + owner);
-            assert.equal(owner, accounts[1], "Not empty !");
+            assert.equal(owner, accounts[1], "Wrong owner !");
             //assert.equal(-1, 1, "Not empty !");
              return instance.deletePlanet(1, {from: accounts[0]}).then(function() {
                return instance.getPlanetCount().then(function(count) {
@@ -47,5 +47,36 @@ contract('ERC721Planet', function(accounts) {
 }); // buy test
 
 
+  it("create, buy, sell, then delete a planet", function() {
+    return Planet.deployed().then(function(instance) {
+      // createPlanet(uint _tokenId, bytes32 _name, string _description, bytes32 _ipfs, uint _price) public
+      return instance.createPlanet(2, "PName", "PDesc", "ipfsaddress", 100, {from: accounts[0]}).then(function(res) {
+        //console.log("createPlanet=" + res);
+        return instance.buyPlanet(2, {from: accounts[2], to :instance.address, gas: 1000000, value:web3.toWei(100, "szabo")}
+      ).then(function(res) {
+            return instance.ownerOf(2).then(function(owner){
+            //console.log("owner=" + owner);
+            assert.equal(owner, accounts[2], "Wrong owner !");
+            //assert.equal(-1, 1, "Not empty !");
+            instance.sellPlanet(2, 500, {from: accounts[2], to :instance.address, gas: 1000000, value:web3.toWei(500, "szabo")}
+          ).then(function() {
+            instance.buyPlanet(2, {from: accounts[3], to :instance.address, gas: 1000000, value:web3.toWei(500, "szabo")}
+          ).then(function(res) {
+                return instance.ownerOf(2).then(function(owner){
+                //console.log("owner=" + owner);
+                assert.equal(owner, accounts[3], "Wrong owner !");
+             return instance.deletePlanet(2, {from: accounts[0]}).then(function() {
+               return instance.getPlanetCount().then(function(count) {
+                   assert.equal(count.valueOf(), 0, "Not empty !");
+                 });
+             });
+             });
+             });
+             });
+          }); //ownerOf
+        });//buy
+    });
+  })
+}); // buy test
 
 });
