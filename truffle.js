f62a01a2c1b1584d3ed4431c33376c7415f95e22
ben <benoitcourty.fr>
2018-04-23 08:19:45 +0000

    Ropsten

--- truffle.js
+++ truffle.js
@@ -6,6 +6,13 @@ module.exports = {
             host: "127.0.0.1",
             port: 8545,
             network_id: "*" // Match any network id
-        }
+        },
+	ropsten:  {
+	     network_id: 3,
+     host: "localhost",
+     port:  8545,
+     gas:   3257178
+
+}
     }
 };
